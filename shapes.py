72ba352a0f37c0e8e2877db497c524c263f9f149
alexander <farbantis@gmail.com>
2022-10-28 11:40:52 +0300

    add calculation of the area of a square

--- shapes.py
+++ shapes.py
@@ -1,5 +1,12 @@
+import math
+
+
 class Shape:
-    def __init__(self, shape, x, y):
+    def __init__(self, shape, x=0, y=0):
         self.shape = shape
         self.x = x
         self.y = y
+
+    def square_area(self):
+        area = math.pi * self.x ** 2
+        return area
